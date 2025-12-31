fe9ee60d4d3858450f8035c8fcad81b96f161685
marekch96 <marek.checinski96@gmail.com>
2025-12-31 16:05:16 +0000

    testing nvidia support

--- train_yolo_model.py
+++ train_yolo_model.py
@@ -1,6 +1,10 @@
 from  ultralytics import YOLO
+import torch
+
 import cv2
 
-model = YOLO('yolo11l.yaml')
 
-results= model.train(data='config.yaml',epochs=50,batch=8,name='rust_detector_v11l')
+print(torch.cuda.is_available())
+#model = YOLO('yolo11l.yaml')
+
+#results= model.train(data='config.yaml',epochs=50,batch=8,name='rust_detector_v11l')
