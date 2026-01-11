0e52c5951ef32f49e407353d5079bd18810f6df7
marekch96 <marek.checinski96@gmail.com>
2026-01-11 12:48:20 +0000

    update to segmentation model with new dataset

--- train_yolo_model.py
+++ train_yolo_model.py
@@ -4,7 +4,7 @@ import torch
 import cv2
 
 
-print(torch.cuda.is_available())
-#model = YOLO('yolo11l.yaml')
+#print(torch.cuda.is_available())
+model = YOLO('yolo11l-seg.yaml')
 
-#results= model.train(data='config.yaml',epochs=50,batch=8,name='rust_detector_v11l')
+results= model.train(data='config.yaml',epochs=100,batch=8,name='rust_detection_new_dataset_seg_b8_e100')
